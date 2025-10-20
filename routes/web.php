0947ddf845eb412d403dd0de790bb03065b842a3
FathurIrwan07 <muhamadfathurirwan@gmail.com>
2025-10-20 15:47:49 +0700

    menambahkan template dashbord admin dan membuat halaman login di layanan pengaduan

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,27 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('app');
+    return view('home');
 });
+
+Route::middleware(['auth', 'verified'])->get('/dashboard', function () {
+    $role = auth()->user()->role;
+
+    return match ($role) {
+        'admin' => redirect()->route('admin.dashboard'),
+        // 'staff' => redirect()->route('staff.dashboard'),
+        // 'user' => redirect()->route('user.dashboard'),
+        default => redirect('/'),
+    };
+})->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
